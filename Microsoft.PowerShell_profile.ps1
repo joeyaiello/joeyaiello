0c6290c23b6ab105bafec3c0983e04833341c676
Joey Aiello <joseph.a.aiello@gmail.com>
2016-04-22 13:44:42 -0700

    fix posh-git cruft

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,26 @@
-# $env:PSModulePath = "C:\WINDOWS\system32\WindowsPowerShell\v1.0\Modules\;C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement;C:\Program Files\WindowsPowerShell\Modules;C:\Users\jaiello\Documents\WindowsPowerShell\Modules"
 
 function subl { &"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args }
-if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
 
 # Make sure ssh-agent/add can be loaded by posh-git
 Set-Alias ssh-agent "$env:ProgramFiles\git\usr\bin\ssh-agent.exe"
 Set-Alias ssh-add "$env:ProgramFiles\git\usr\bin\ssh-add.exe"
 
 # Load posh-git example profile
-. 'C:\tools\poshgit\dahlbyk-posh-git-7acc70b\profile.example.ps1'
+Import-Module posh-git
 
-Rename-Item Function:\Prompt PoshGitPrompt -Force
-function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
+# Create posh-git prompt
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    Write-Host($pwd.ProviderPath) -nonewline
+    Write-VcsStatus
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "> "
+}
 
+# Load Invoke-Locate
 $env:NODE_PATH = Join-Path -Path $env:ProgramFiles -ChildPath 'nodejs\node_modules\'
 New-Alias -name locate -value C:\Users\jaiello\AppData\Local\locate\Invoke-Locate.ps1 -scope Global -force
 New-Alias -name updatedb -value C:\Users\jaiello\AppData\Local\locate\Update-LocateDB.ps1 -scope Global -force
-
-# Load posh-git example profile
-. 'C:\Users\jaiello\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 
 bash -c '/usr/games/fortune | /usr/games/cowsay -f tux'
 
