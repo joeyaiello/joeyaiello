7e2819e87c34b91922df162d6cb3e58e3f24d650
joeyaiello <joseph.a.aiello@gmail.com>
2016-05-13 10:09:47 -0700

    add touch function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -20,6 +20,9 @@ function global:prompt {
 New-Alias -name locate -value C:\Users\jaiello\AppData\Local\locate\Invoke-Locate.ps1 -scope Global -force
 New-Alias -name updatedb -value C:\Users\jaiello\AppData\Local\locate\Update-LocateDB.ps1 -scope Global -force
 
+# touch
+function touch { New-Item -Type File $args }
+
 bash -c '/usr/games/fortune | /usr/games/cowsay -f tux'
 
 # All last minute PATH additions 
