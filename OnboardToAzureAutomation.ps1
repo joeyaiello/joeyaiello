8844b518b8764c0c0c30eda4c7b7c652e54ae407
joeyaiello <joseph.a.aiello@gmail.com>
2017-02-12 16:05:59 -0800

    add basic 'onboard to AzureAutomation' script

---
+++ OnboardToAzureAutomation.ps1
@@ -0,0 +1,16 @@
+﻿Login-AzureRmAccount
+
+# Define the parameters for Get-AzureRmAutomationDscOnboardingMetaconfig using PowerShell Splatting
+$Params = @{
+    ResourceGroupName = 'jaaa'; # The name of the ARM Resource Group that contains your Azure Automation Account
+    AutomationAccountName = 'jaaa-account'; # The name of the Azure Automation Account where you want a node on-boarded to
+    ComputerName = @('localhost'); # The names of the computers that the meta configuration will be generated for
+    OutputFolder = $PSScriptRoot;
+}
+
+# Use PowerShell splatting to pass parameters to the Azure Automation cmdlet being invoked
+# For more info about splatting, run: Get-Help -Name about_Splatting
+Get-AzureRmAutomationDscOnboardingMetaconfig @Params
+$MetaConfigFolder = (Join-Path $Params.OutputFolder 'DscMetaConfigs')
+Set-DscLocalConfigurationManager -Path $MetaConfigFolder
+Remove-Item $MetaConfigFolder 
