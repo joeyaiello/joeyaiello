6af9beb4387168124eb89b3e580390c0a4661f8a
joeyaiello <joseph.a.aiello@gmail.com>
2017-03-19 00:51:37 -0700

    remove locate from profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,10 +16,6 @@ function global:prompt {
     return "> "
 }
 
-# Load Invoke-Locate
-New-Alias -name locate -value C:\Users\jaiello\AppData\Local\locate\Invoke-Locate.ps1 -scope Global -force
-New-Alias -name updatedb -value C:\Users\jaiello\AppData\Local\locate\Update-LocateDB.ps1 -scope Global -force
-
 # touch
 function touch { New-Item -Type File $args }
 
@@ -39,4 +35,3 @@ $env:path += ";C:\tools\nssm-2.24\win64"
 
 # Add F5 key binding for PSReadline's CaptureScreen
 Set-PSReadlineKeyHandler -Function CaptureScreen -Chord Ctrl+[
-
