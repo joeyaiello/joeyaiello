1e23e16f9f24f39c7966cd7dbc9c3d4f68ca21af
joeyaiello <joseph.a.aiello@gmail.com>
2017-03-28 11:27:07 -0700

    fix cp for Linux box

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ $profileLocation = Split-Path -Parent $PROFILE
 if (!(Test-Path $profileLocation)) {
    mkdir $profileLocation 
 }
-cp .\Microsoft.PowerShell_profile.ps1 $profile
+Copy-Item .\Microsoft.PowerShell_profile.ps1 $profile
