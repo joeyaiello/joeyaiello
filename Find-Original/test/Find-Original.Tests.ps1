7994483a942db183ed09030958f428dc1d9610c8
joeyaiello <joseph.a.aiello@gmail.com>
2017-04-16 20:12:42 -0700

    start on tests

--- Find-Original/test/Find-Original.Tests.ps1
+++ Find-Original/test/Find-Original.Tests.ps1
@@ -8,3 +8,12 @@ Describe 'Module Manifest Tests' {
     }
 }
 
+Import-Module $ModuleManifestPath
+
+Describe 'Unit Tests' {
+    It 'finds uniquely named files' {
+        $correct = Get-Item "$PSScriptRoot\mockTarget\bar.txt"
+        $result = Find-UniqueFile -Source "$PSScriptRoot\mockSource" -Target "$PSScriptRoot\mockTarget"
+        $result | Should Be $correct
+    }
+}
