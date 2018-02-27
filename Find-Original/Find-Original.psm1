f6f7199827833947eaa8725148f51240843bb3e9
joeyaiello <joseph.a.aiello@gmail.com>
2018-02-27 00:33:03 -0800

    more stash of busted code (I think)

--- Find-Original/Find-Original.psm1
+++ Find-Original/Find-Original.psm1
@@ -1,3 +1,31 @@
+function GetLengthDifferenceFromMatchingFilenames {
+    param(
+        [Parameter(Mandatory)]
+        [System.Object[]]
+        $a,
+        [Parameter(Mandatory)]
+        [System.Object[]]
+        $b
+    )
+
+    $output = New-Object System.Collections.ArrayList
+
+    foreach ($i in $b) {
+        foreach ($j in $a) {
+            if (($i.Name -eq $j.Name) -and ($i.Length -ne $j.Length)) {
+                $output.Add([pscustomobject]@{
+                    Name = $i.Name
+                    SourceLength = $j.Length
+                    TargetLength = $i.Length
+                    LengthDifference = ($j.Length)-($i.Length)
+               }) | Out-Null
+            }
+        }
+    }
+
+    return $output
+}
+
 function Find-UniqueFile {
 
     [CmdletBinding()]
@@ -32,22 +60,25 @@ function Find-UniqueFile {
     $TargetFiles = Get-ChildItem -Recurse $Target
 
     if ($BySize -like 'Both') {
-        $UniqueFilenames = Compare-Object $SourceFiles $TargetFiles -Property PSChildName,Length -PassThru | Where-Object SideIndicator -like '=>'
+        #$UniqueFilenames = Compare-Object $SourceFiles $TargetFiles -Property PSChildName,Length -PassThru
+        GetLengthDifferenceFromMatchingFilenames -a $SourceFiles -b $TargetFiles
     }
     elseif ($BySize -like 'Larger') {
         $UniqueFilenames = Compare-Object $SourceFiles $TargetFiles -Property PSChildName,Length -PassThru #| Where-Object SideIndicator -like '=>'
         1
     }
     else {
-#        $UniqueFilenames = (Compare-Object $SourceFiles.PSChildName $TargetFiles | ? SideIndicator -like '=>').InputObject
-         $UniqueFilenames = Compare-Object $SourceFiles $TargetFiles -Property PSChildName -PassThru | Where-Object SideIndicator -like '=>'
+        #$UniqueFilenames = (Compare-Object $SourceFiles.PSChildName $TargetFiles | ? SideIndicator -like '=>').InputObject
+        $UniqueFilenames = Compare-Object $SourceFiles $TargetFiles -Property PSChildName -PassThru | Where-Object SideIndicator -like '=>'
+        $TargetFiles | Where-Object {$UniqueFilenames.PSChildName.Contains($_.PSChildName)}
     }
 
     #$UnqiueFiles = foreach($i in $UniqueFilenames) {
     #    $UniqueFile = $TargetFiles | Where-Object PSChildName -like $i.PSChildName
     #}
 
-    $TargetFiles | Where-Object {$UniqueFilenames.PSChildName.Contains($_.PSChildName)}
 }
+
+
 
 Export-ModuleMember -Function Find-UniqueFile
