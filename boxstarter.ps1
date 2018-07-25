848412fa79bce097142897109e474a0b639c2e6c
joeyaiello <joseph.a.aiello@gmail.com>
2018-07-24 17:32:06 -0700

    initial boxstarter script

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,6 +1,18 @@
+
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 Enable-RemoteDesktop
+Enable-MicrosoftUpdate
+# I think I want Install-WindowsUpdate *first*, but I'm not sure yet
+# Install-WindowsUpdate
+# Also, why not use built-in WindowsUpdateProvider?
 
+Disable-BingSearch
+Disable-InternetExplorerESC
+# Figure out Move-LibraryDirectory and Get-LibraryNames
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -DisableOpenFileExplorerToQuickAccess -DisableShowRecentFilesInQuickAccess -DisableShowFrequentFoldersInQuickAccess -EnableShowRibbon
+Set-TaskbarOptions -Size Small -UnLock -Dock Bottom -Combine Never
+Enable-RemoteDesktop
+Update-ExecutionPolicy -policy Bypass
 cinst git --params="'/GitOnlyOnPath /NoShellIntegration /SChannel'"
 cinst vscode --params ="'/NoDesktopIcon'"
 cinst vscode-powershell
