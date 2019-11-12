d55c685cb9386934248070f2d35c9b3f48151e49
joeyaiello <joseph.a.aiello@gmail.com>
2019-11-12 14:12:44 -0800

    comment boxstarter for 1st try MVP

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,20 +1,31 @@
 Enable-MicrosoftUpdate
 # I think I want Install-WindowsUpdate *first*, but I'm not sure yet
 # Install-WindowsUpdate
-# Also, why not use built-in WindowsUpdateProvider?
+# TODO: why not use built-in WindowsUpdateProvider?
 
 Disable-BingSearch
 Disable-InternetExplorerESC
-# Figure out Move-LibraryDirectory and Get-LibraryNames
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -DisableOpenFileExplorerToQuickAccess -DisableShowRecentFilesInQuickAccess -DisableShowFrequentFoldersInQuickAccess -EnableShowRibbon
+# TODO: Figure out Move-LibraryDirectory and Get-LibraryNames
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles`
+                           -EnableShowFileExtensions -DisableOpenFileExplorerToQuickAccess`
+                           -DisableShowRecentFilesInQuickAccess -DisableShowFrequentFoldersInQuickAccess`
+                           -EnableShowRibbon
 Set-TaskbarOptions -Size Small -UnLock -Dock Bottom -Combine Never
-Enable-RemoteDesktop
 Update-ExecutionPolicy -policy Bypass
 
 cinst git --params="'/GitOnlyOnPath /NoShellIntegration /SChannel'"
 cinst vscode --params ="'/NoDesktopIcon'"
 cinst vscode-powershell
-Install-Module posh-git
+cinst microsoft-edge-insider-dev
 
+## Module Installation
+
+# Hold off on module installs until I figure out PS7
+# Install-Module posh-git
+
+# Temporarily turning off Windows feature enablement for remoting
+<#
 cinst Microsoft-Hyper-V-All -source windowsFeatures
 Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'
+Enable-RemoteDesktop
+#>
