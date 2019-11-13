a2da9ad2c4851a888635acae52ba4f1cd67c84d9
joeyaiello <joseph.a.aiello@gmail.com>
2019-11-12 17:44:57 -0800

    add a bunch of TODO stubs

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -14,9 +14,16 @@ Set-TaskbarOptions -Size Small -UnLock -Dock Bottom -Combine Never
 Update-ExecutionPolicy -policy Bypass
 
 cinst git --params="'/GitOnlyOnPath /NoShellIntegration /SChannel'"
+
+### VS code
 cinst vscode --params ="'/NoDesktopIcon'"
+# TODO: replace this with an eventual code invocation that gets the preview extension
 cinst vscode-powershell
+
+### Chromium Edge Dev
 cinst microsoft-edge-insider-dev
+# TODO: how do I install extensions?
+# TODO: is it enough to login with MSA in Win10 to get non-extensions?
 
 ## Module Installation
 
